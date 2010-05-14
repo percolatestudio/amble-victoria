a89ca8e3d620cba08426b9b9322475239cfe5832
Joe Dollard <jdollard@gmail.com>
2010-05-13 23:19:48 -0700

    move js from page to view level js

--- public/javascripts/views/places/quickedit.js
+++ public/javascripts/views/places/quickedit.js
@@ -1,7 +1,31 @@
 (function($){
   placesQuickedit = {
-    onReady: function() {
-      alert('called!');
+    onReady: function() {      
+      $(document).keypress(function(event) {
+        var chr =  String.fromCharCode( event.which );
+
+        if ((chr >= 0) && (chr <= 10)) {
+          var img_select = jQuery('#primary_image_select').get(0);
+
+          if (chr < img_select.length) {
+            img_select.selectedIndex = chr;
+
+            jQuery('#primary_image ul img').removeClass('active');
+            jQuery('#place_image' + chr).addClass('active')
+          } 
+        }
+        else if (chr == 'h') {
+          var uq_select = jQuery('#user_quality_select').get(0);
+          uq_select.selectedIndex = 1;
+        }
+        else if (chr == 's') {
+          jQuery('form').submit();
+        }
+        else if (chr == 'e') {
+          window.location.pathname = jQuery('#edit').attr('href');
+        }
+
+      });
     }
   }
 }(jQuery));
