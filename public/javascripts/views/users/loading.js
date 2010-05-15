c6c801b728c95bb72a5fa6d4c7c93f25e64ae8b6
Joe Dollard <jdollard@gmail.com>
2010-05-15 16:29:34 -0700

    work around FF geolocation bug

--- public/javascripts/views/users/loading.js
+++ public/javascripts/views/users/loading.js
@@ -1,5 +1,7 @@
 (function($){
   usersLoading = {
+    onGetLocationCompleteCalled: false,
+    
     onReady: function() {
       // make sure all links target the content pane      
       $('a:not(.fbconnect_login_button):not(.noTargetContent)').targetContent('#content');
@@ -16,6 +18,12 @@
     },
     
     onGetLocationComplete: function(request_data) {
+      //work around a bug in Firefox 3.5 where the geolocaiton callback 
+      //sometimes gets called multiple times
+      if (usersLoading.onGetLocationCompleteCalled == true) {
+        return;
+      }
+      
       //update user location, and load places
       jQuery.ajax({
         url: '/users/update_location',
@@ -24,7 +32,9 @@
           $.loadContent('#content', '/places/');
         },
         error: function() {  alert("error setting your location") }
-      });      
+      }); 
+      
+      usersLoading.onGetLocationCompleteCalled = true;     
     }
   }
 }(jQuery));
