a4d4824d00f1b9fe8bf051d69a4ba7ad69a62d38
Tom Coleman <tom@thesnail.org>
2010-05-19 14:47:49 -0700

    scroll the location bar out of view

---
+++ public/javascripts/views/layouts/mobile.js
@@ -0,0 +1,11 @@
+(function($){
+  layoutsMobile = {
+    onLoad: function() {
+      // scroll the location bar out view
+      setTimeout(function() {
+        window.scrollTo(0,1);
+        window.scrollTo(0,0);
+      }, 100);
+    }
+  }
+}(jQuery));
