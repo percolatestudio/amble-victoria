d35c77131e238a3af0f58fe00d621267651cfce6
Zoltan Olah <zol@me.com>
2010-05-18 23:32:44 -0700

    Working copy of atdw script

--- data/atdw.rb
+++ data/atdw.rb
@@ -20,20 +20,133 @@ class Atdw < Base
   end
   
   def normalize
-    load
+    load_products(read_product_ids)
+    # TEST
+    # xml = IO.read('product.xml')
+    # @records << make_record(xml)
   end
   
-  def generate_product_ids(filename = 'atdw_ids.yaml')
+  def load_products(ids)
+    ids.each_with_index do |id, i|
+      productQuery = {
+        'DistributorKey' => @key,
+        'CommandName' => 'GetProduct',
+        'CommandParameters' =><<EOL
+<parameters>
+  <row><param>DATA_GROUP_LIST</param><value>ALL</value></row>
+  <row><param>PRODUCT_ID</param><value>#{id}</value></row>
+</parameters>
+EOL
+      }
+      puts "--- Running productQuery for id: #{id} (#{i} of #{ids.length} products)"
+      result = @proxy.CommandHandler(productQuery)
+      
+      begin
+        @records << make_record(result.commandHandlerResult)
+      rescue Exception => e
+        puts "HOLY SHIT! product: #{id} exploded with e: #{e.to_s}"
+      end
+    end
+  end
+  
+  def make_record(xml)
+    doc = Nokogiri::XML(xml)
+    r = {}
+    r[:category_name] = 'Attraction'
+    r[:source_name] = 'Tourism Vic Gov Data'
+    
+    doc.xpath("//product_record").each { |product|
+      name = product.at_xpath(".//product_name")
+      description = product.at_xpath(".//product_description")
+
+      r[:name] = capitalize_words(name.inner_text) unless name.nil?
+      r[:description] = description.inner_text unless description.nil?
+    }      
+
+    doc.xpath("//product_address").each { |address|
+      address_line_1 = address.at_xpath(".//address_line_1")
+      address_line_2 = address.at_xpath(".//address_line_2")
+      address_line_3 = address.at_xpath(".//address_line_3")
+      city_name = address.at_xpath(".//city_name")
+      address_postal_code = address.at_xpath(".//address_postal_code")
+      geocode_gda_latitude = address.at_xpath(".//geocode_gda_latitude")
+      geocode_gda_longitude = address.at_xpath(".//geocode_gda_longitude")
+      
+      loc_arry = []
+      loc_arry << address_line_1.inner_text unless address_line_1.nil?
+      loc_arry << address_line_2.inner_text unless address_line_2.nil?
+      loc_arry << address_line_3.inner_text unless address_line_3.nil?
+      loc_arry << city_name.inner_text unless city_name.nil?
+      loc_arry << address_postal_code.inner_text unless address_postal_code.nil?
+
+      r[:location] = join_address(loc_arry, 0, loc_arry.length)
+      r[:lng] = geocode_gda_longitude.inner_text unless geocode_gda_longitude.nil?
+      r[:lat] = geocode_gda_latitude.inner_text unless geocode_gda_latitude.nil?      
+    }
+    
+    doc.xpath("//product_communication").each { |comm|
+      comm.xpath(".//row").each { |row|
+        id = row.at_xpath(".//attribute_id_communication")
+        
+        unless id.nil?
+          if id.inner_text == 'CAPHENQUIR'
+            communication_area_code = row.at_xpath(".//communication_area_code")
+            communication_detail = row.at_xpath(".//communication_detail")
+            
+            if communication_area_code and communication_detail
+              r[:phone] = "(#{communication_area_code.inner_text}) #{communication_detail.inner_text}"
+            end
+          end
+          
+          if id.inner_text == 'CAURENQUIR'
+            communication_detail = row.at_xpath(".//communication_detail")
+            
+            if communication_detail
+              r[:webpages_attributes] = [{:url => 'http://' + communication_detail.inner_text}]
+            end
+          end
+        end
+      }
+    }
+ 
+    doc.xpath("//product_multimedia").each { |comm|
+      comm.xpath(".//row").each { |row|
+        type = row.at_xpath(".//attribute_id_multimedia_content")
+        orientation = row.at_xpath(".//attribute_id_size_orientation")
+        url = row.at_xpath(".//server_path")
+        
+        unless type.nil? or orientation.nil? or url.nil?
+          if type.inner_text == 'IMAGE' and orientation.inner_text == 'LANDSCAPE'
+              if r[:images_attributes].nil?
+                r[:images_attributes] = [{:url => url.inner_text}]
+              else
+                r[:images_attributes] << {:url => url.inner_text}
+              end
+          end
+        end
+      }
+    }    
+        
+    if r.empty?
+      puts "Hmmm, empty record found, maybe there is a problem. XML was: #{xml}"
+    end
+    
+    r
+  end
+  
+  def read_product_ids(filename = 'atdw_ids.yaml')
+    YAML.load_file(filename)
+  end
+  
+  def write_product_ids(filename = 'atdw_ids.yaml')
+    ids = []
+    
     mainQuery = {
       'DistributorKey' => @key,
       'CommandName' => 'QueryProducts',
       'CommandParameters' =><<EOL
 <parameters>
   <row><param>PRODUCT_CATEGORY_LIST</param><value>ATTRACTION</value></row>
-  <row><param>DATA_GROUP_LIST</param><value>ALL</value></row>
-  <row><param>ADDRESS_RETURN</param><value>YES</value></row>
-  <row><param>MULTIMEDIA_RETURN</param><value>YES</value></row>
-  <row><param>MULTIMEDIA_TYPE</param><value>LANDSCAPE</value></row>  
   <row><param>STATE</param><value>Victoria</value></row>
   <row><param>RESULTS_PER_PAGE</param><value>#{@rpp}</value></row>
 </parameters>
@@ -48,10 +161,10 @@ EOL
     puts "Total pages found: #{total_pages}"
     puts "Records per page: #{@rpp}"
         
-    @records += find_products(result.commandHandlerResult)
+    ids += find_ids(result.commandHandlerResult)
     
-    # 2.upto(total_pages) do |page|
-    2.upto(2) do |page_number|
+    2.upto(total_pages) do |page_number|
+    # 2.upto(2) do |page_number|
       pageQuery = {
         'DistributorKey' => @key,
         'CommandName' => 'QueryProductsNextPage',
@@ -65,38 +178,27 @@ EOL
       }
       puts "--- Running pageQuery for page #{page_number}"
       result = @proxy.CommandHandler(pageQuery)
-      @records += find_products(result.commandHandlerResult)
+      ids += find_ids(result.commandHandlerResult)
     end
+    
+    # write the bad boys to disk
+    File.open(filename, 'w' ) { |out| YAML.dump ids, out }
+    puts "Wrote #{ids.length} ids"
   end
-  
-  def find_products(xml)
+
+  def find_ids(xml)
     doc = Nokogiri::XML(xml)
-    products = []
+    ids = []
     
     doc.xpath("//product_record").each { |product|
-      p = {}
-      
-      p[:id] = product.at_xpath(".//product_id").inner_text
-      # r[:category_name] = 'Culture'
-      # r[:source_name] = 'Art Spaces Gov Data'
-      #       
-      # r[:name] = row[0].to_s
-      # r[:location] = "#{row[1]}, #{row[2]}, #{row[3]}"
-      # r[:webpages_attributes] = [{:url => row[4].to_s}]
-      
-      
-      products << p
+      ids << product.at_xpath(".//product_id").inner_text
     }
     
-    if products.empty?
-      puts "Hmmm, no products found, maybe there is a problem. XML was: #{xml}"
+    if ids.empty?
+      puts "Hmmm, no ids found, maybe there is a problem. XML was: #{xml}"
     end
     
-    out_s = open('out.xml', "wb")
-    out_s.write(xml)
-    out_s.close
-    
-    products
+    ids
   end
   
   def find_stats(xml)
@@ -115,3 +217,6 @@ EOL
 end
 
 Base.main(Atdw.new)
+
+# a = Atdw.new
+# a.write_product_ids
