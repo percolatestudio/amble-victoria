f55606d1c0e2bee982a3f9d885347b7fa86cb7b2
Zoltan Olah <zol@me.com>
2010-05-19 23:41:05 -0700

    Added layout+nav for website

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,15 @@
 class StaticController < ApplicationController
+  def home
+    redirect_to explore_url if is_mobile_device?
+  end
+    
   def test
+    current_navigation :explore
+    
     respond_to do |format|
       format.html 
       format.mobile { return render :action => 'test.html.erb' }
     end
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,14 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resources :places, :collection => [:quickedit], :member => [:save, :unsave]
   map.explore 'explore', :controller => :places, :action => :index
-  map.root :explore
 
   map.resource :user_session
   map.resources :users, :collection => [:get_location, :set_location, :my_places, :account, :update_location]
   map.my_places 'my_places', :controller => :users, :action => :my_places
   
   map.about 'about', :controller => :static, :action => :about
+  
+  map.root :controller => :static, :action => :home
   
   # The priority is based upon order of creation: first created -> highest priority.
 
